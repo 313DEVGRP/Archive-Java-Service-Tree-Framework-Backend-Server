bb1f16e249542e4c4af01ba890862dcdad2ccc1a
LeeDongMin <admin@313.co.kr>
2017-07-17 13:39:51 +0900

    [BT] SP-474 #close #time 1h #comment dwr version push complete [RV] +review SR @admin

---
+++ standard/project/web/src/main/java/egovframework/rivalwar/api/menu/service/MenuServiceImpl.java
@@ -0,0 +1,64 @@
+package egovframework.rivalwar.api.menu.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import egovframework.com.ext.jstree.springHibernate.core.service.JsTreeHibernateService;
+import egovframework.com.ext.jstree.springHibernate.core.vo.JsTreeHibernateSearchDTO;
+
+@Service("MenuService")
+public class MenuServiceImpl implements MenuService {
+
+	@Autowired
+	private JsTreeHibernateService jsTreeHibernateService;
+	
+	@Override
+	public <T extends JsTreeHibernateSearchDTO> T getMenu(T jsTreeHibernateDTO) throws Exception {
+		return jsTreeHibernateService.getNode(jsTreeHibernateDTO);
+	}
+
+	@Override
+	public <T extends JsTreeHibernateSearchDTO> List<T> getChildMenu(T jsTreeHibernateDTO) throws Exception {
+		return jsTreeHibernateService.getChildNode(jsTreeHibernateDTO);
+	}
+	
+	@Override
+	public <T extends JsTreeHibernateSearchDTO> List<T> getPaginatedChildMenu(T jsTreeHibernateDTO) throws Exception {
+		return jsTreeHibernateService.getPaginatedChildNode(jsTreeHibernateDTO);
+	}
+
+	@Override
+	public <T extends JsTreeHibernateSearchDTO> List<String> searchMenu(T jsTreeHibernateDTO) throws Exception {
+		return jsTreeHibernateService.searchNode(jsTreeHibernateDTO);
+	}
+
+	@Override
+	public <T extends JsTreeHibernateSearchDTO> T addMenu(T jsTreeHibernateDTO) throws Exception {
+		return jsTreeHibernateService.addNode(jsTreeHibernateDTO);
+	}
+
+	@Override
+	public <T extends JsTreeHibernateSearchDTO> int removeMenu(T jsTreeHibernateDTO) throws Exception {
+		return jsTreeHibernateService.removeNode(jsTreeHibernateDTO);
+	}
+
+	@Override
+	public <T extends JsTreeHibernateSearchDTO> int alterMenu(T jsTreeHibernateDTO) throws Exception {
+		return jsTreeHibernateService.alterNode(jsTreeHibernateDTO);
+	}
+
+	@Override
+	public <T extends JsTreeHibernateSearchDTO> int alterMenuType(T jsTreeHibernateDTO) throws Exception {
+		return jsTreeHibernateService.alterNodeType(jsTreeHibernateDTO);
+	}
+
+	@Override
+	public <T extends JsTreeHibernateSearchDTO> T moveMenu(T jsTreeHibernateDTO, HttpServletRequest request)
+			throws Exception {
+		return jsTreeHibernateService.moveNode(jsTreeHibernateDTO, request);
+	}
+}
---
+++ standard/project/web/src/main/java/egovframework/rivalwar/api/menu/vo/MenuDTO.java
@@ -0,0 +1,91 @@
+package egovframework.rivalwar.api.menu.vo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.SelectBeforeUpdate;
+
+import egovframework.com.ext.jstree.springHibernate.core.vo.JsTreeHibernateSearchDTO;
+
+@Entity
+@Table(name = "T_JSTREE_MENU")
+@SelectBeforeUpdate(value = true)
+@DynamicInsert(value = true)
+@DynamicUpdate(value = true)
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@SequenceGenerator(name = "JsTreeSequence", sequenceName = "S_JSTREE_MENU", allocationSize = 1)
+public class MenuDTO extends JsTreeHibernateSearchDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5641929581490357881L;
+
+	public MenuDTO() {
+		super();
+	}
+
+	public MenuDTO(Boolean copyBooleanValue) {
+		super();
+		this.copyBooleanValue = copyBooleanValue;
+	}
+
+	/*
+	 * Extend Bean Field
+	 */
+	private Boolean copyBooleanValue;
+
+	@Transient
+	public Boolean getCopyBooleanValue() {
+		copyBooleanValue = false;
+		if (this.getCopy() == 0) {
+			copyBooleanValue = false;
+		} else {
+			copyBooleanValue = true;
+		}
+		return copyBooleanValue;
+	}
+
+	public void setCopyBooleanValue(Boolean copyBooleanValue) {
+		this.copyBooleanValue = copyBooleanValue;
+	}
+
+	private String c_vote_start_date;
+	private String c_vote_end_date;
+
+	@Column(name = "c_vote_start_date")
+	public String getC_vote_start_date() {
+		return c_vote_start_date;
+	}
+
+	public void setC_vote_start_date(String c_vote_start_date) {
+		this.c_vote_start_date = c_vote_start_date;
+	}
+
+	@Column(name = "c_vote_end_date")
+	public String getC_vote_end_date() {
+		return c_vote_end_date;
+	}
+
+	public void setC_vote_end_date(String c_vote_end_date) {
+		this.c_vote_end_date = c_vote_end_date;
+	}
+
+	@Override
+	public <T extends JsTreeHibernateSearchDTO> void setFieldFromNewInstance(T paramInstance) {
+		if (paramInstance instanceof MenuDTO) {
+			this.setC_vote_start_date(this.getC_vote_start_date());
+			this.setC_vote_end_date(this.getC_vote_end_date());
+		}
+	}
+
+}
