d67fb3856e7e0fa5e38af419c8d2330d9fa92824
LeeDongMin <admin@313.co.kr>
2017-08-31 11:22:34 +0900

    [BT] SP-486 #close #time 1h #comment aggregate controller 스켈레톤 [RV] +review SR @admin

--- standard/project/web/src/main/java/egovframework/api/rivalWar/aggregateResult/controller/AnonymousAggregateResultController.java
+++ standard/project/web/src/main/java/egovframework/api/rivalWar/aggregateResult/controller/AnonymousAggregateResultController.java
@@ -1,7 +1,22 @@
 package egovframework.api.rivalWar.aggregateResult.controller;
 
+import egovframework.api.rivalWar.aggregateResult.service.AggregateResultService;
+import egovframework.com.ext.jstree.support.mvc.GenericAbstractController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Administrator on 2017-08-29.
  */
-public class AnonymousAggregateResultController {
+@Controller
+@RequestMapping(value = {"/api/rivalWar/aggregateResult"})
+public class AnonymousAggregateResultController extends GenericAbstractController {
+
+    @Autowired
+    private AggregateResultService aggregateResultService;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 }
--- standard/project/web/src/main/java/egovframework/api/rivalWar/aggregateResult/controller/UserAggregateResultController.java
+++ standard/project/web/src/main/java/egovframework/api/rivalWar/aggregateResult/controller/UserAggregateResultController.java
@@ -1,7 +1,23 @@
 package egovframework.api.rivalWar.aggregateResult.controller;
 
+import egovframework.api.rivalWar.aggregateResult.service.AggregateResultService;
+import egovframework.api.rivalWar.menu.service.MenuService;
+import egovframework.com.ext.jstree.support.mvc.GenericAbstractController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Administrator on 2017-08-29.
  */
-public class UserAggregateResultController {
+@Controller
+@RequestMapping(value = {"/api/rivalWar/ROLE_USER/aggregateResult"})
+public class UserAggregateResultController extends GenericAbstractController {
+
+    @Autowired
+    private AggregateResultService aggregateResultService;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 }
