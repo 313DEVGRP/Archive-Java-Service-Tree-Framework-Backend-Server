e189bf62684d0812c8cdd1dff84833c96edb2c16
LeeDongMin <admin@313.co.kr>
2017-10-08 18:29:10 +0900

    [BT] SP-490 #close #time 1h #comment Compare item service dto Push [RV] +review SR @admin

---
+++ standard/project/web/src/main/java/egovframework/api/rivalWar/compareItem/service/CompareItemServiceImpl.java
@@ -0,0 +1,7 @@
+package egovframework.api.rivalWar.compareItem.service;
+
+/**
+ * Created by Administrator on 2017-10-08.
+ */
+public class CompareItemServiceImpl {
+}
---
+++ standard/project/web/src/main/java/egovframework/api/rivalWar/compareItem/vo/CompareItemDTO.java
@@ -0,0 +1,7 @@
+package egovframework.api.rivalWar.compareItem.vo;
+
+/**
+ * Created by Administrator on 2017-10-08.
+ */
+public class CompareItemDTO {
+}
