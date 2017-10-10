a08a9a06c86742a382011140f1f00e1cad27c259
LeeDongMin <admin@313.co.kr>
2017-10-10 17:47:36 +0900

    [BT] SP-490 #close #time 1h #comment 인터페이스를 코어에서부터 확장. [RV] +review SR @admin

--- standard/project/web/src/main/java/egovframework/api/rivalWar/menu/service/MenuService.java
+++ standard/project/web/src/main/java/egovframework/api/rivalWar/menu/service/MenuService.java
@@ -1,8 +1,28 @@
 package egovframework.api.rivalWar.menu.service;
 
-import egovframework.com.ext.jstree.springHibernate.core.service.JsTreeHibernateService;
+import egovframework.com.ext.jstree.springHibernate.core.vo.JsTreeHibernateSearchDTO;
 
-public interface MenuService extends JsTreeHibernateService {
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+public interface MenuService {
 	
+	public <T extends JsTreeHibernateSearchDTO> T getNode(T jsTreeHibernateDTO) throws Exception;
+
+	public <T extends JsTreeHibernateSearchDTO> List<T> getChildNode(T jsTreeHibernateDTO) throws Exception;
+
+	public <T extends JsTreeHibernateSearchDTO> List<T> getPaginatedChildNode(T jsTreeHibernateDTO) throws Exception;
+
+	public <T extends JsTreeHibernateSearchDTO> List<String> searchNode(T jsTreeHibernateDTO) throws Exception;
+
+	public <T extends JsTreeHibernateSearchDTO> T addNode(T jsTreeHibernateDTO) throws Exception;
+
+	public <T extends JsTreeHibernateSearchDTO> int removeNode(T jsTreeHibernateDTO) throws Exception;
+
+	public <T extends JsTreeHibernateSearchDTO> int alterNode(T jsTreeHibernateDTO) throws Exception;
+
+	public <T extends JsTreeHibernateSearchDTO> int alterNodeType(T jsTreeHibernateDTO) throws Exception;
+
+	public <T extends JsTreeHibernateSearchDTO> T moveNode(T jsTreeHibernateDTO, HttpServletRequest request) throws Exception;
 
 }
