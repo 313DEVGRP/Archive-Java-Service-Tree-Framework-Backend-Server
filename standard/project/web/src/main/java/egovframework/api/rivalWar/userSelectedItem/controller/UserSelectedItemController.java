c2d0d651ee3784f7874fe480ab337c61cbc257c5
LeeDongMin <admin@313.co.kr>
2017-10-18 17:34:33 +0900

    [BT] SP-490 #close #time 1h #comment jsTree DirectChat Controller Service push [RV] +review SR @admin

--- standard/project/web/src/main/java/egovframework/api/rivalWar/userSelectedItem/controller/UserSelectedItemController.java
+++ standard/project/web/src/main/java/egovframework/api/rivalWar/userSelectedItem/controller/UserSelectedItemController.java
@@ -1,7 +1,22 @@
 package egovframework.api.rivalWar.userSelectedItem.controller;
 
+import egovframework.api.rivalWar.specHashTag.service.SpecHashTagService;
+import egovframework.com.ext.jstree.support.mvc.GenericAbstractController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Administrator on 2017-10-14.
  */
-public class UserSelectedItemController {
+@Controller
+@RequestMapping(value = {"/api/rivalWar/ROLE_USER/userSelectedItem"})
+public class UserSelectedItemController extends GenericAbstractController {
+
+    @Autowired
+    private SpecHashTagService specHashTagService;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 }
