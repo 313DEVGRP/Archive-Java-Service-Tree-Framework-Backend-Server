f1aa5c0c4b9eb92dac26aa95c86f7b1dd53f9eb4
LeeDongMin <admin@313.co.kr>
2018-09-22 13:10:08 +0900

    RivalWar Service seperate

---
+++ web-RivalWar/src/main/java/egovframework/api/rivalWar/directChat/service/DirectChatServiceImpl.java
@@ -0,0 +1,9 @@
+package egovframework.api.rivalWar.directChat.service;
+
+import egovframework.com.ext.jstree.springHibernate.core.service.JsTreeHibernateServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service("DirectChatService")
+public class DirectChatServiceImpl extends JsTreeHibernateServiceImpl implements DirectChatService {
+
+}
---
+++ web-RivalWar/src/main/java/egovframework/api/rivalWar/userSelectedItem/service/UserSelectedItemService.java
@@ -0,0 +1,7 @@
+package egovframework.api.rivalWar.userSelectedItem.service;
+
+import egovframework.com.ext.jstree.springHibernate.core.service.JsTreeHibernateService;
+
+public interface UserSelectedItemService extends JsTreeHibernateService {
+
+}
