3d226e2a0362ff5addaed10a4dca806c80a2762a
LeeDongMin <admin@313.co.kr>
2018-12-14 20:46:53 +0900

    [ TSF Core-Module 분리 및 Web-Module 독립 운영 ]
    [SmartCommit] SP-535 #close #time 1h +review SR @dongmin.lee
    #comment TSF의 설정 파일을 META-INF로 이동했으며,
    독립적인 jar파일로 dependency를 구성하여 프로젝트에 적용할 수 있도록
    구조를 변경하고 반영함.
    
    [Document] http://www.313.co.kr/confluence/display/WIKI/1.0+jsTree+Service+Framework
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-535
    [VersionControl] https://github.com/jstree/jsTree-Service-Framework-Backend-App
    [CodeReview] http://www.313.co.kr/fecru/changelog/jsTreeServiceFrameworkBackendAppRepo
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/4772
    [Docker] https://hub.docker.com/r/313devgrp/jstree-service-framework-backend-app/

--- web-rivalwar/src/main/java/egovframework/api/rivalWar/specHashTag/service/SpecHashTagService.java
+++
@@ -1,6 +0,0 @@
-package egovframework.api.rivalWar.specHashTag.service;
-
-import egovframework.com.ext.jstree.springHibernate.core.service.JsTreeHibernateService;
-
-public interface SpecHashTagService extends JsTreeHibernateService{
-}
