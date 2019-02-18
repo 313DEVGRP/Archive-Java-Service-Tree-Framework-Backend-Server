e16b431729424da92303b32f38d407f896f1522f
LeeDongMin <admin@313.co.kr>
2019-02-18 18:03:34 +0900

    [ jsTree Service Framework(TSF) - Dev Portal Component package replace]
    [SmartCommit] SP-535 #close #time 1h +review SR @dongmin.lee
    #comment TSF의 Dev Portal class package replace
    
    [Document] http://www.313.co.kr/confluence/display/WIKI/1.0+jsTree+Service+Framework
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-535
    [VersionControl] https://github.com/jstree/jsTree-Service-Framework-Backend-App
    [CodeReview] http://www.313.co.kr/fecru/changelog/jsTreeServiceFrameworkBackendAppRepo
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/4772
    [Docker] https://hub.docker.com/r/313devgrp/jstree-service-framework-backend-app/

--- web-module/src/main/java/egovframework/api/web/menu/service/MenuService.java
+++
@@ -1,7 +0,0 @@
-package egovframework.api.web.menu.service;
-
-import egovframework.com.ext.jstree.springHibernate.core.service.JsTreeHibernateService;
-
-public interface MenuService extends JsTreeHibernateService{
-
-}
