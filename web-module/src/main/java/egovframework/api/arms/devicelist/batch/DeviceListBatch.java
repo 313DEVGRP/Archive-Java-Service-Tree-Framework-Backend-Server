0f7ef39333f28e3dd0c93ee36f26ed40acc74ac4
LeeDongMin <admin@313.co.kr>
2021-01-12 18:46:05 +0900

    [ Developer Portal ( Based By ARMS ) - Backend ]
    [SmartCommit] SP-586 #close #time 1h +review SR @dongmin.lee #comment update datasource to grafana, version upgrade
    
    [Document] http://www.313.co.kr/confluence/display/WIKI
    [IssueTracker] http://www.313.co.kr/jira/browse/SP-586
    [VersionControl] https://github.com/jstree/JavaServiceTree-Frontend-WebProject
    [CodeReview] http://www.313.co.kr/fecru/changelog/jsTreeServiceFrameworkFrontendAppRepo
    [BuildManager] http://www.313.co.kr/bamboo/browse/STAN-RC
    [ArtifactManager] http://www.313.co.kr/nexus/content/repositories/StandardProject/
    [CodeAnalysis] http://www.313.co.kr/sonar/dashboard/index/4772
    [DockerRepository] https://hub.docker.com/r/313devgrp/jstree-service-framework-backend-app/

--- web-module/src/main/java/egovframework/api/arms/devicelist/batch/DeviceListBatch.java
+++ web-module/src/main/java/egovframework/api/arms/devicelist/batch/DeviceListBatch.java
@@ -1,15 +1,11 @@
 package egovframework.api.arms.devicelist.batch;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import egovframework.api.arms.devicelist.service.DeviceListService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Administrator on 2020-11-22.
@@ -17,25 +13,16 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class DeviceListBatch {
 
+    @Autowired
+    private DeviceListService deviceListService;
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    //@Scheduled(fixedDelay=60000*5, initialDelay =10000)
-    //@Scheduled(cron="0/10 0 * * * ?")
-    //@Scheduled(fixedDelay=60000*1)
-    public void updateDeviceHost() {
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setReadTimeout(5000); // 읽기시간초과, ms
-        factory.setConnectTimeout(3000); // 연결시간초과, ms
-        HttpClient httpClient = HttpClientBuilder.create()
-                .setMaxConnTotal(100) // connection pool 적용
-                .setMaxConnPerRoute(5) // connection pool 적용
-                .build();
-        factory.setHttpClient(httpClient); // 동기실행에 사용될 HttpClient 세팅
-        RestTemplate restTemplate = new RestTemplate(factory);
+    @Scheduled(fixedDelay=60000*5, initialDelay =10000)
+    public void updateDeviceHost() throws Exception {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        Integer updateCount = deviceListService.updateDeviceListToJstree();
+        logger.info("DeviceList update Count = " + updateCount);
 
-        restTemplate.getForEntity("http://localhost:28080/api/v1/devicelist/restController/{action}", String.class, "update");
     }
 }
