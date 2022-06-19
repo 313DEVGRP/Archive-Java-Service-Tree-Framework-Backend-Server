31f577cbf90e07612619c0de0a07b1b1b6852201
LeeDongMin <admin@313.co.kr>
2022-06-19 22:38:05 +0900

    [SP-313] #comment 분석 탭, 요구사항 탭 DSL 코드 푸쉬 및 버전 업 #close #time 1h +review SR @dongmin.lee
    [ Backend-Server::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

---
+++ web-module/src/main/java/egovframework/api/arms/module_analpdheatmap/service/AnalPdHeatmap.java
@@ -0,0 +1,20 @@
+/*
+ * @author Dongmin.lee
+ * @since 2022-06-19
+ * @version 22.06.19
+ * @see <pre>
+ *  Copyright (C) 2007 by 313 DEV GRP, Inc - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by 313 developer group <313@313.co.kr>, December 2010
+ * </pre>
+ */
+package egovframework.api.arms.module_analpdheatmap.service;
+
+import egovframework.com.ext.jstree.springHibernate.core.service.JsTreeHibernateService;
+
+public interface AnalPdHeatmap extends JsTreeHibernateService {
+
+
+
+}
---
+++ web-module/src/main/java/egovframework/api/arms/module_analreqjira/controller/UserAnalReqJiraController.java
@@ -0,0 +1,53 @@
+/*
+ * @author Dongmin.lee
+ * @since 2022-06-19
+ * @version 22.06.19
+ * @see <pre>
+ *  Copyright (C) 2007 by 313 DEV GRP, Inc - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by 313 developer group <313@313.co.kr>, December 2010
+ * </pre>
+ */
+package egovframework.api.arms.module_analreqjira.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import egovframework.com.ext.jstree.springHibernate.core.controller.SHVAbstractController;
+
+import egovframework.api.arms.module_analreqjira.model.AnalReqJiraDTO;
+import egovframework.api.arms.module_analreqjira.service.AnalReqJira;
+
+@Slf4j
+@Controller
+@RequestMapping(value = {"/auth-user/api/arms/analreqjira"})
+public class UserAnalReqJiraController extends SHVAbstractController<AnalReqJira, AnalReqJiraDTO> {
+
+    @Autowired
+    @Qualifier("analReqJira")
+    private AnalReqJira analReqJira;
+
+    @PostConstruct
+    public void initialize() {
+        setJsTreeHibernateService(analReqJira);
+    }
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
