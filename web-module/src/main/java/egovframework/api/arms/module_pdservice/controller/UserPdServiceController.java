79901aa92959daf45ca73114c064a91cbbb9f91e
LeeDongMin <admin@313.co.kr>
2022-11-15 22:18:41 +0900

    [SP-313] #comment Requirement 카테고리 :: 등록관리 메뉴 :: 22.11.15 트리거 생성 처리 완료 #close #time 1h +review SR @dongmin.lee
    [Backend-Server::Java-Service-Tree-Framework(JSTF)::313DEVGRP]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- web-module/src/main/java/egovframework/api/arms/module_pdservice/controller/UserPdServiceController.java
+++ web-module/src/main/java/egovframework/api/arms/module_pdservice/controller/UserPdServiceController.java
@@ -46,6 +46,10 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -71,6 +75,9 @@ public class UserPdServiceController extends SHVAbstractController<PdService, Pd
 
     @Resource(name = "reqAddSqlMapper")
     ReqAddSqlMapper reqAddSqlMapper;
+
+    @Resource(name = "egov.dataSource")
+    DataSource dataSource;
 
     @PostConstruct
     public void initialize() {
@@ -115,7 +122,7 @@ public class UserPdServiceController extends SHVAbstractController<PdService, Pd
             }else{
                 reqAddSqlMaaperDTO.setC_title(C_title_org);
                 reqAddSqlMapper.ddlLogExecuteToReqAdd(reqAddSqlMaaperDTO);
-                //reqAddSqlMapper.ddlTriggerLogSqlExecuteToReqAdd(reqAddSqlMaaperDTO);
+                makeTrigger(reqAddSqlMaaperDTO);
             }
 
             //C_ETC 컬럼에 요구사항 테이블 이름 기입
@@ -134,6 +141,60 @@ public class UserPdServiceController extends SHVAbstractController<PdService, Pd
             modelAndView.addObject("result", addedNode);
             return modelAndView;
         }
+    }
+
+    private void makeTrigger(ReqAddSqlMaaperDTO reqAddSqlMaaperDTO) throws SQLException {
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+        String sql =
+                "CREATE OR REPLACE TRIGGER \"TRIG_" + reqAddSqlMaaperDTO.getC_title() + "\"\n" +
+                        "BEFORE DELETE OR INSERT OR UPDATE\n" +
+                        "ON " + reqAddSqlMaaperDTO.getC_title() + " REFERENCING NEW AS NEW OLD AS OLD\n" +
+                        "FOR EACH ROW\n" +
+                        "DECLARE\n" +
+                        "tmpVar NUMBER;\n" +
+                        "/******************************************************************************\n" +
+                        "   NAME:       TRIGGER_COMPREHENSIVETREE\n" +
+                        "   PURPOSE:    \n" +
+                        " \n" +
+                        "   REVISIONS:\n" +
+                        "   Ver        Date        Author           Description\n" +
+                        "   ---------  ----------  ---------------  ------------------------------------\n" +
+                        "   1.0        2012-08-29             1. Created this trigger.\n" +
+                        " \n" +
+                        "   NOTES:\n" +
+                        " \n" +
+                        "   Automatically available Auto Replace Keywords:\n" +
+                        "      Object Name:     TRIGGER_COMPREHENSIVETREE\n" +
+                        "      Sysdate:         2012-08-29\n" +
+                        "      Date and Time:   2012-08-29, 오후 5:26:44, and 2012-08-29 오후 5:26:44\n" +
+                        "      Username:         (set in TOAD Options, Proc Templates)\n" +
+                        "      Table Name:      T_ARMS_REQADD (set in the \"New PL/SQL Object\" dialog)\n" +
+                        "      Trigger Options:  (set in the \"New PL/SQL Object\" dialog)\n" +
+                        "******************************************************************************/\n" +
+                        "BEGIN\n" +
+                        "  tmpVar := 0;\n" +
+                        "   IF UPDATING  THEN    \n" +
+                        "       insert into " + reqAddSqlMaaperDTO.getC_title() + "_LOG (C_ID,C_PARENTID,C_POSITION,C_LEFT,C_RIGHT,C_LEVEL,C_TITLE,C_TYPE,C_METHOD,C_STATE,C_DATE)\n" +
+                        "       values (:old.C_ID,:old.C_PARENTID,:old.C_POSITION,:old.C_LEFT,:old.C_RIGHT,:old.C_LEVEL,:old.C_TITLE,:old.C_TYPE,'update','변경이전데이터',sysdate);        \n" +
+                        "       insert into " + reqAddSqlMaaperDTO.getC_title() + "_LOG (C_ID,C_PARENTID,C_POSITION,C_LEFT,C_RIGHT,C_LEVEL,C_TITLE,C_TYPE,C_METHOD,C_STATE,C_DATE)\n" +
+                        "       values (:new.C_ID,:new.C_PARENTID,:new.C_POSITION,:new.C_LEFT,:new.C_RIGHT,:new.C_LEVEL,:new.C_TITLE,:new.C_TYPE,'update','변경이후데이터',sysdate);   \n" +
+                        "    END IF;\n" +
+                        "   IF DELETING THEN\n" +
+                        "       insert into " + reqAddSqlMaaperDTO.getC_title() + "_LOG (C_ID,C_PARENTID,C_POSITION,C_LEFT,C_RIGHT,C_LEVEL,C_TITLE,C_TYPE,C_METHOD,C_STATE,C_DATE)\n" +
+                        "       values (:old.C_ID,:old.C_PARENTID,:old.C_POSITION,:old.C_LEFT,:old.C_RIGHT,:old.C_LEVEL,:old.C_TITLE,:old.C_TYPE,'delete','삭제된데이터',sysdate);\n" +
+                        "   END IF;   \n" +
+                        "   IF INSERTING  THEN\n" +
+                        "       insert into " + reqAddSqlMaaperDTO.getC_title() + "_LOG (C_ID,C_PARENTID,C_POSITION,C_LEFT,C_RIGHT,C_LEVEL,C_TITLE,C_TYPE,C_METHOD,C_STATE,C_DATE)\n" +
+                        "       values (:new.C_ID,:new.C_PARENTID,:new.C_POSITION,:new.C_LEFT,:new.C_RIGHT,:new.C_LEVEL,:new.C_TITLE,:new.C_TYPE,'insert','삽입된데이터',sysdate);\n" +
+                        "   END IF;\n" +
+                        " \n" +
+                        "  EXCEPTION\n" +
+                        "    WHEN OTHERS THEN\n" +
+                        "      -- Consider logging the error and then re-raise\n" +
+                        "      RAISE;\n" +
+                        "END TRIG_" + reqAddSqlMaaperDTO.getC_title() + ";";
+        statement.execute(sql);
     }
 
     @ResponseBody
